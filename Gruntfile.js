b493af09c69a4f88cc7da47a96a761b753cbb106
Webbiker <info@webbiker.nl>
2017-02-08 09:23:04 +0100

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,87 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      js: {
+        files : {
+          'assets/js/build/app.min.js' : [
+            'assets/js/partials/app.js'
+          ]
+        }
+      },
+      options: {
+        sourceMap: true,
+        sourceMapName: 'assets/js/build/app.min.map'
+      },
+    },
+
+    postcss: {
+      options: {
+        map: true,
+        processors: [
+          require('autoprefixer')({
+            browsers: ['last 2 versions', 'ie 8', 'ie 9', '> 1%']}
+          )
+        ]
+      },
+      dist: {
+        src: 'assets/css/screen.css'
+      }
+    },
+
+    sass: {                              // Task
+      dist: {                            // Target
+        options: {                       // Target options
+          style: 'compressed'            // nested, compact, compressed, expanded
+        },
+        files: {                         // Dictionary of files
+          'assets/css/screen.css': 'assets/sass/screen.scss'
+        }
+      }
+    },
+
+    watch: {
+      css: {
+        files: ['assets/sass/*.scss', 'assets/sass/partials/*.scss'],
+        tasks: ['sass', 'postcss']
+      },
+      scripts: {
+          files: ['assets/js/*.js', 'assets/js/partials/*.js'],
+          tasks: ['uglify']
+      }
+    },
+
+    browserSync: {
+        dev: {
+            bsFiles: {
+                src : [
+                    'assets/css/*.css',
+                    'assets/js/*.js',
+                    '**/*.html',
+                    '*.html',
+                    '**/*.php',
+                    '*.php'
+                ]
+            },
+            options: {
+                watchTask: true,
+                proxy: "local.register.jeugd3daagse.nl"
+            }
+        }
+    }
+});
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-postcss');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-browser-sync');
+
+  grunt.registerTask('default', ['browserSync', 'watch']);
+
+};
---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html class="no-js">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title></title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        
+        <!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+        <link rel="apple-touch-icon-precomposed" href="/assets/img/favicon/apple-touch-icon-114x114-precomposed.png" sizes="114x114">
+        <link rel="apple-touch-icon-precomposed" href="/assets/img/favicon/apple-touch-icon-72x72-precomposed.png" sizes="72x72">
+        <link rel="icon" type="image/x-icon" href="/assets/img/favicon/favicon.ico">
+
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.2.1/jquery.min.js"></script>
+
+		<link rel="stylesheet" href="/assets/css/screen.css">
+    </head>
+    <body>
+        <header class="header"></header>
+        <main class="main" id="main-content">
+
+		</main>
+		<footer></footer>
+		<script src="/assets/js/build/app.min.js"></script>
+	</body>
+</html>
